f0ef35b39a01711e89050fdda701054b119a0824
lester <servqua88@gmail.com>
2019-11-21 22:31:14 +0800

    Create project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const exphbs = require('express-handlebars')
+const bodyParse = require('body-parser')
+
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParse.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`app is running on:http://localhost:${port}`)
+})
+
