46682af3f841864ba8ea29ffd4215f15308845d6
lester <servqua88@gmail.com>
2019-11-27 00:36:10 +0800

    Connect to mongodb

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const app = express()
 
 const exphbs = require('express-handlebars')
 const bodyParse = require('body-parser')
+const mongoose = require('mongoose')
 
 const port = 3000
+
+// 連線mongodb
+mongoose.connect('mongodb://127.0.0.1/shortURL', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
